c3ed322ace9487c19819d802c04dd017992089a7
Patrick Brown <patiek@gmail.com>
2015-09-07 11:52:13 -0700

    Add dialog prompt, m3u support, dynamic download time
    Add dialog prompt that prompts for format in-page.
    Add m3u support that allows for generating an m3u-style playlist.
    Add dynamic download time that restarts download countdown after a new song is found.
    
    Add soundiiz.com usage to readme.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Google Music playlist exporter allowing Google Play Music playlist export with javascript.
 
 # About
-This code allows you to export your Google Music playlist into a CSV file or a text file, allowing potential import into other applications (such as Spotify).
+This code allows you to export your Google Music playlist into an M3U playlist file, CSV file, or a text file, allowing potential import into other applications (such as Spotify, rdio, SoundCloud, Tidal, Deezer, Groove, Napster).
 
 # Usage
 ## Basic Usage
@@ -10,18 +10,28 @@ This code allows you to export your Google Music playlist into a CSV file or a t
 2. Navigate to the playlist you want to export in google music and ensure you have not scrolled down the page.
 3. Open your browser's inspector window (in Chrome, right click > inspect element).
 4. Copy & paste the javascript from exporter.js into the console tab of your inspector window and press enter.
-5. Scroll down the page at a good pace, the exporter will scrape the playlist as you scroll.
-6. After 30 seconds, the exporter will automatically initialize a file download of your playlist file.
+5. View the page, a dialog prompt will ask you what type of playlist you would like to generate.
+6. Scroll down the page, the exporter will scrape the playlist as you scroll, indicating number of songs scraped in lower right of browser.
+7. After no new songs have been scraped for 10 seconds, the exporter will automatically initialize a file download of your playlist file.
 
-## Exporting to Spotify
+## Importing Generated Playlist to Other Services
+There are two options for importing the generated playlist:
+#### Import using Soundiiz
+1. Use [Soundiiz](http://soundiiz.com/) to connect to other music services (such as Spotify, rdio, SoundCloud, Tidal, Deezer, Groove, Napster).
+2. Drag & Drop the generated m3u playlist file from Basic Usage steps above into Soundiiz.
+3. Soundiiz will process your file and prompt you to select the services you connected in step 1 as the destination of your playlist.
+
+#### Import using Ivy
 1. Perform the steps under Basic Usage.
 2. Use [Ivy](http://www.ivyishere.org/) to create a playlist file for Spotify from the playlist.txt file using the Copy/Paste functionality of Ivy to copy the text from the playlist.txt file.
 
 ## Options
 The javascript takes two arguments which you will find on the last line:
 #### maxTime 
-##### default: 30
-This is the time in seconds that the script waits for you to scroll the playlist before downloading the file.
+##### default: -1
+This is the time in seconds that the script waits for you to scroll the playlist before downloading the file. 
+A -1 value indicates to wait until on new songs have been found for 10 seconds.
 #### format
-##### default: ivy
-This is the format the script will download the file as. Supports 'ivy' and 'csv'. If you need a custom format, you should download as a CSV and then open with spreadsheet software such as Excel or Google Docs.
+##### default: prompt
+This is the format the script will download the file as. Supports 'prompt', 'm3u', 'ivy' and 'csv'. The 'prompt' value will generate an in-page dialog asking your to specify. 
+If you need a custom format, you should download as a CSV and then open with spreadsheet software such as Excel or Google Docs.
--- exporter.js
+++ exporter.js
@@ -1,9 +1,14 @@
-(function (maxWait,format){
+(function (maxWait,plstype){
 
     var self = this,
         downloaded = false,
         playlistEntries={},
-        totalWait=0;
+        format = plstype,
+        dialog;
+
+    this.totalWait = 0;
+    this.totalWaitSinceChange = 0;
+    this.totalSongs = 0;
 
     this.quoteEscape = function(text) {
         return '"'+text.replace('"','""')+'"';
@@ -16,16 +21,45 @@
                 title = l.querySelectorAll('td[data-col="title"] .content')[0].textContent,
                 artist = l.querySelectorAll('td[data-col="artist"] .content')[0].textContent,
                 album = l.querySelectorAll('td[data-col="album"] .content')[0].textContent,
+                duration = l.querySelectorAll('td[data-col="duration"] span')[0].textContent,
                 lineFormat;
-            if (format=='ivy') {
-                lineFormat = artist.replace('-',' ').trim() + ' - ' + title.replace(/(.+?)\s*[\(\[].+[\)\]]\s*$/, '$1').trim();
+            if (self.format == 'ivy') {
+                lineFormat = artist.replace('-', ' ').trim() + ' - ' + title.replace(/(.+?)\s*[\(\[].+[\)\]]\s*$/, '$1').trim();
+            } else if (self.format == 'm3u') {
+                var durationParts = duration.split(':'),
+                    totalSeconds;
+
+                if (durationParts.length === 3) {
+                    totalSeconds = parseInt(durationParts[0],10) * 3600 + parseInt(durationParts[1],10) * 60 + parseInt(durationParts[2],10);
+                } else if (durationParts.length == 2) {
+                    totalSeconds = parseInt(durationParts[0],10) * 60 + parseInt(durationParts[1],10);
+                } else {
+                    totalSeconds = -1;
+                }
+
+                lineFormat = '#EXTINF:' + totalSeconds + ','
+                    + artist.replace('-', ' ').trim()
+                    + ' - '
+                    + title.replace(/(.+?)\s*[\(\[].+[\)\]]\s*$/, '$1').trim()
+                    + "\n"
+                    + album.trim()
+                    + '/'
+                    + artist.replace('-', ' ').trim()
+                    + ' - '
+                    + title.replace(/(.+?)\s*[\(\[].+[\)\]]\s*$/, '$1').trim()
+                    + '.mp3'
             } else {
-                lineFormat = quoteEscape(artist)+','+quoteEscape(title)+','+quoteEscape(album);
+                lineFormat = quoteEscape(artist)+','+quoteEscape(title)+','+quoteEscape(album)+','+quoteEscape(duration);
             }
-            playlistEntries[lineFormat] = {
-                artist: artist,
-                title: title,
-                album: album
+            if (!playlistEntries.hasOwnProperty(lineFormat)) {
+                self.totalSongs++;
+                self.totalWaitSinceChange = 0;
+                playlistEntries[lineFormat] = {
+                    artist: artist,
+                    title: title,
+                    album: album,
+                    duration: duration
+                }
             }
         }
     };
@@ -42,9 +76,13 @@
         for (var line in playlistEntries) {
             csvContent += line + "\n";
         }
-        if (format=='ivy') {
+        if (self.format == 'ivy') {
             type = 'text';
             filename = 'playlist.txt';
+        } else if (self.format == 'm3u') {
+            csvContent = "#EXTM3U\n" + csvContent;
+            type = 'text/m3u';
+            filename = 'playlist.m3u';
         } else {
             type = 'text/csv';
             filename = 'playlist.csv';
@@ -58,14 +96,50 @@
 
     this.scrollAll = function() {
         self.scrape();
-        if (!downloaded && totalWait < maxWait*1000) {
-            totalWait += 200; // close enough
-            setTimeout(function(){self.scrollAll();}, 200);
-        } else if (!downloaded) {
-          download();
+        if (downloaded) {
+            return;
+        }
+        var timeUntilDownload = maxWait >= 0 ? maxWait*1000 - self.totalWait: 9000 - self.totalWaitSinceChange;
+        if (timeUntilDownload > 0) {
+            self.totalWait += 50; // close enough
+            self.totalWaitSinceChange += 50;
+            self.statusBox.innerHTML = 'Download in ' + Math.round(timeUntilDownload/1000) + ' seconds; Songs: ' + self.totalSongs;
+            setTimeout(function(){self.scrollAll();}, 50);
+        } else {
+            download();
         }
     };
 
-    this.scrape();
-    this.scrollAll();
-})(30,'ivy');
+    this.begin = function() {
+        self.dialog.close();
+        var playlistSelect = document.getElementById('playlist-format');
+        self.format = playlistSelect.options[playlistSelect.selectedIndex].value;
+        self.scrape();
+        self.scrollAll();
+        return false;
+    };
+
+    self.totalSongs = 0;
+    self.statusBox = document.createElement('div');
+    self.statusBox.style.position = 'fixed';
+    self.statusBox.style.bottom = '0';
+    self.statusBox.style.right = '0';
+    self.statusBox.style.background = '#CCC';
+    self.statusBox.style.zIndex = 999999;
+    self.statusBox.innerHTML = 'Songs: 0';
+    document.body.appendChild(self.statusBox);
+
+    if (plstype === 'prompt') {
+        self.dialog = document.createElement('dialog');
+        self.dialog.innerHTML = '<form onsubmit="return self.begin()"><strong>Select Format:</strong><select id="playlist-format">'
+            +'<option value="m3u">M3U (Generic, soundiiz.com, etc.)</option>'
+            +'<option value="csv">CSV (Generic CSV)</option>'
+            +'<option value="ivy">IVY (ivyishere.org)</option>'
+            +'</select> <input type="submit" value="Begin"/></dialog>';
+        document.body.appendChild(self.dialog);
+        self.dialog.showModal();
+    } else {
+        self.begin();
+    }
+
+})(-1,'prompt');
